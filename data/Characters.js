fb97a70071dcdd813fe84b267e509a47c3ff14b7
caurin <gabrielcaurin.564@gmail.com>
2023-10-16 14:02:43 -0300

    feat: css fixed and more limit in js

--- data/Characters.js
+++ data/Characters.js
@@ -8,21 +8,21 @@ const API_PRIVATE_KEY = "d57d50281d8ac0e805135173184c87e419b4eec1";
 const getTimeStamp = () => Date.now().toString();
 
 const generateHash = (timeStamp) => {
-  const hash = md5(timeStamp + API_PRIVATE_KEY + API_PUBLIC_KEY);
-  return hash;
+    const hash = md5(timeStamp + API_PRIVATE_KEY + API_PUBLIC_KEY);
+    return hash;
 };
 
 export const getCharacters = async () => {
-  const timeStamp = getTimeStamp();
-  const hash = generateHash(timeStamp);
-  const url = `${API_BASE_URL}/characters?ts=${timeStamp}&apikey=${API_PUBLIC_KEY}&hash=${hash}`;
+    const timeStamp = getTimeStamp();
+    const hash = generateHash(timeStamp);
+    const url = `${API_BASE_URL}/characters?ts=${timeStamp}&apikey=${API_PUBLIC_KEY}&hash=${hash}&limit=100&offset=0`;
 
 
-  try {
-    const resp = await axios.get(url);
-    return resp.data.data.results;
-  } catch (error) {
-    throw error;
-  }
-  }
+    try {
+        const resp = await axios.get(url);
+        return resp.data.data.results;
+    } catch (error) {
+        throw error;
+    }
+}
 
