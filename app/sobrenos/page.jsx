718706cdfbf02a60554dc0c5bf5a301e16d01ff8
MatheusScaranello <matheusscaranello.564@gmail.com>
2023-10-16 17:16:26 -0300

    aaa

--- app/sobrenos/page.jsx
+++ app/sobrenos/page.jsx
@@ -6,10 +6,10 @@ const Sobrenos = () => {
         <div className={styles.container}>
             <h1 className={styles.title}>Sobre nós</h1>
             <div className={styles.cardContainer}>
-                <IntegranteCash nome="Gabriel" cargo="CEO" foto="https://avatars.githubusercontent.com/u/69424154?v=4" description="Gabriel é o CEO da empresa e é responsável por gerenciar a equipe e as tarefas." />
-                <IntegranteCash nome="João" cargo="CTO" foto="https://avatars.githubusercontent.com/u/69424154?v=4" description="João é o CTO da empresa e é responsável por gerenciar a equipe e as tarefas." />
-                <IntegranteCash nome="Maria" cargo="CFO" foto="https://avatars.githubusercontent.com/u/69424154?v=4" description="Maria é o CFO da empresa e é responsável por gerenciar a equipe e as tarefas." />
-                <IntegranteCash nome="José" cargo="CIO" foto="https://avatars.githubusercontent.com/u/69424154?v=4" description="José é o CIO da empresa e é responsável por gerenciar a equipe e as tarefas." />
+                <IntegranteCash nome="Gabriel" cargo="CEO" foto=".png" description="Gabriel é o CEO da empresa e é responsável por gerenciar a equipe e as tarefas." />
+                <IntegranteCash nome="João" cargo="CTO" foto=".png" description="João é o CTO da empresa e é responsável por gerenciar a equipe e as tarefas." />
+                <IntegranteCash nome="Maria" cargo="CFO" foto=".png" description="Maria é o CFO da empresa e é responsável por gerenciar a equipe e as tarefas." />
+                <IntegranteCash nome="José" cargo="CIO" foto=".png" description="José é o CIO da empresa e é responsável por gerenciar a equipe e as tarefas." />
             </div>
         </div>
     )
