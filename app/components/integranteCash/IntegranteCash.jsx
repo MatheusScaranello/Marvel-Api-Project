372d7e11c38f37028c2051a2bf1d2aaa0d93d730
MatheusScaranello <matheusscaranello.564@gmail.com>
2023-10-16 16:50:19 -0300

    feat integrantesCash

---
+++ app/components/integranteCash/IntegranteCash.jsx
@@ -0,0 +1,14 @@
+import styles from './IntegranteCash.module.css'
+
+const IntegranteCash = (nome, cargo , foto, description) => {
+    return (
+        <div className={styles.card}>
+        <h3 className={styles.name}>{nome}</h3>
+        <h4 className={styles.cargo}>{cargo}</h4>
+        <img src={foto} alt={nome} className={styles.img} />
+        <p className={styles.desc}>{description}</p>
+        </div>
+    )
+    }
+
+    export default IntegranteCash
