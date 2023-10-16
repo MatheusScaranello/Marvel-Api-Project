3345ec3b07ee5840ae3f561af0f684a260757572
MatheusScaranello <matheusscaranello.564@gmail.com>
2023-10-16 13:55:25 -0300

    feat header

---
+++ app/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './header.module.css'
+
+const Header = () => {
+    return (
+        <div className={styles.header}>
+        <h1 className={styles.title}>Welcome to my website</h1>
+        </div>
+    )
+    }
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Header from '../components/header/header'
 
 export default function Home() {
   return (
     <>
+      <Header />
     </>
   )
 }
