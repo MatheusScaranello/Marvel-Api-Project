78151ce81d6783f74e12928111e0eabea5ad237a
MatheusScaranello <matheusscaranello.564@gmail.com>
2023-10-16 16:00:32 -0300

    feat footer

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }
