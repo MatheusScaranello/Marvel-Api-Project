7cc6d007a494e1eec6924c1251cfbc328d87b880
caurin <gabrielcaurin.564@gmail.com>
2023-10-20 17:06:25 -0300

    aaa

--- app/createCharacters/page.jsx
+++ app/createCharacters/page.jsx
@@ -1,9 +1,13 @@
 import CreateCharacters from "../components/createCharacters/CreateCharacters"
+import Header from "../components/header/Header"
+import Footer from "../components/footer/Footer"
 
 export default function teste() {
     return (
         <>
+            <Header />
             <CreateCharacters />
+            <Footer />
         </>
     )
 }
