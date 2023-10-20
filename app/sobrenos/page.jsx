6b5e8a91ba6ad0b42aaded0ca5134e475c1e5f80
marinamorais <marinamorais.564@gmail.com>
2023-10-20 17:03:03 -0300

    aa

--- app/sobrenos/page.jsx
+++ app/sobrenos/page.jsx
@@ -10,7 +10,7 @@ const Sobrenos = () => {
                 <IntegranteCash nome="Matheus Scaranello" cargo="TechLeader" foto="https://i.imgur.com/8K96q0J.jpg" description="João é o CTO da empresa e é responsável por gerenciar a equipe" />
                 <IntegranteCash nome="Caique Naimi" cargo="CTO" foto="https://i.imgur.com/174eKbW.jpg" description="João é o CTO da empresa e é responsável por gerenciar a equipe e as tarefas." />
                 <IntegranteCash nome="Gabriel Canrin" cargo="CFO" foto="https://i.imgur.com/iO9H6V1.jpg" description="Maria é o CFO da empresa e é responsável por gerenciar a equipe e as tarefas." />
-                <IntegranteCash nome="Gabriel Picirile" cargo="CIO" foto="https://i.imgur.com/GUvnMes.jpg" description="José é o CIO da empresa e é responsável por gerenciar a equipe e as tarefas." />
+                <IntegranteCash nome="Gabriel Picirili" cargo="CIO" foto="https://i.imgur.com/GUvnMes.jpg" description="José é o CIO da empresa e é responsável por gerenciar a equipe e as tarefas." />
                 <IntegranteCash nome="Marina Elis" cargo="CIO" foto="https://i.imgur.com/EdCWCfd.jpg" description="José é o CIO da empresa e é responsável por gerenciar a equipe e as tarefas." />
                 <IntegranteCash nome="Stefanny oliveira" cargo="CFO" foto="https://i.imgur.com/KQcoF2u.jpg" description="Maria é o CFO da empresa e é responsável por gerenciar a equipe e as tarefas." />
             </div>
