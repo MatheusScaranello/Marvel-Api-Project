640f832da1934b7cd718acf2c4e1427e7c581fc9
Caique <caiquenaimi.564@gmail.com>
2023-10-20 08:07:45 -0300

    finish componetization searchPage

--- app/searchPage/page.jsx
+++ app/searchPage/page.jsx
@@ -1,16 +1,17 @@
 "use client"
 import { useState } from 'react';
-import { TailSpin } from 'react-loader-spinner';
 import styles from './searchPage.module.css';
 import { useEffect } from 'react';
-import { getCharacters , getCharactersRadom } from '@/data/Characters';
+import { getCharacters, getCharactersRadom } from '@/data/Characters';
+import CardCharacters from '../components/cardCharacters/CardCharacters';
+
 
 function Home() {
   const [apiData, setApiData] = useState([]);
   const [apiDataRadom, setApiDataRadom] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [flag, setFlag] = useState(false);
-  
+
   useEffect(() => {
     const fetchCharacters = async () => {
       try {
@@ -33,13 +34,13 @@ function Home() {
       console.error(error);
     }
   };
-  
+
 
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       handleSearch();
     }
-    
+
   };
 
   return (
@@ -60,95 +61,7 @@ function Home() {
         </div>
       </div>
 
-      <div className={styles.grid}>
-        {
-          
-        
-       flag ? apiData.length > 0 ? apiData ? (
-          apiData.map((item) => (
-            <div className={styles.card} key={item.id}>
-              <div className={styles.front}>
-                <img
-                  src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
-                  alt={item.name}
-                  className={styles.img}
-                />
-                <h3 className={styles.name}>{item.name}</h3>
-              </div>
-              <div className={`${styles.info} ${styles.back}`}>
-                {item.description ? ( 
-                  <p className={styles.desc}>Descrição: {item.description}</p>
-                ) : (
-                  <p className={styles.noDescription}>
-                    Esse personagem não possui descrição
-                  </p>
-                )}
-              </div>
-            </div>
-          ))
-        ) : (
-          <div className={styles.spinner}>
-            <TailSpin
-              height="80"
-              width="80"
-              color="#ffffff"
-              ariaLabel="tail-spin-loading"
-              radius="1"
-              wrapperStyle={{}}
-              wrapperClass=""
-              visible={true}
-            />
-          </div>
-        ) : (
-          <div className={styles.nothing}>
-            <h1 className={styles.nothingText}>
-              Nenhum personagem encontrado
-            </h1>
-          </div>
-        ) : (
-          apiDataRadom ? (
-            apiDataRadom.map((item) => {
-              if (item.description && item.thumbnail.path && item.thumbnail.extension && item.name && item.id) {
-                return(
-                  <div className={styles.card} key={item.id}>
-              <div className={styles.front}>
-                <img
-                  src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
-                  alt={item.name}
-                  className={styles.img}
-                />
-                <h3 className={styles.name}>{item.name}</h3>
-              </div>
-              <div className={`${styles.info} ${styles.back}`}>
-                {item.description ? ( 
-                  <p className={styles.desc}>Descrição: {item.description}</p>
-                ) : (
-                  <p className={styles.noDescription}>
-                    Esse personagem não possui descrição
-                  </p>
-                )}
-              </div>
-            </div>
-                )
-              }
-              
-})
-        ) : (
-          <div className={styles.spinner}>
-            <TailSpin
-              height="80"
-              width="80"
-              color="#ffffff"
-              ariaLabel="tail-spin-loading"
-              radius="1"
-              wrapperStyle={{}}
-              wrapperClass=""
-              visible={true}
-            />
-          </div>
-        )
-        )}
-      </div>
+      <CardCharacters apiData={apiData} apiDataRadom={apiDataRadom} flag={flag} />
     </div>
   );
 }
