e606cd69c7a4cd23dac09321a6d8c88603a4e772
MatheusScaranello <matheusscaranello.564@gmail.com>
2023-10-21 15:56:04 -0300

    AA

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -1,5 +1,8 @@
 import styles from './footer.module.css'
 import NavLink from "../NavLink/NavLink"
+import {BsGithub} from "react-icons/bs"
+import {SiInstagram, SiLinktree} from "react-icons/si"
+import {BsLinkedin} from "react-icons/bs"
 
 
 const Footer = () =>{
@@ -12,17 +15,9 @@ return(
             <h6>Sobre</h6>
             <p class="text-justify">O Marvel Character Hub é o seu portal para explorar o vasto universo de personagens da Marvel. Neste site, você pode mergulhar nas histórias e aventuras de seus heróis e vilões favoritos, além de criar seu próprio personagem e imaginar novas histórias no universo Marvel.</p>
           </div>
-
+      <br />
           <div className={styles.col}>
             <h6>Categorias</h6>
-            <ul className={styles.footer}>
-              <li>Home</li>
-              <li>Sobre nós</li>
-            </ul>
-          </div>
-      
-          <div className={styles.col}>
-            <h6>Quick Links</h6>
             <ul className={styles.footer}>
               <li><NavLink texto="Home" rota="/"/></li>
               <li><NavLink texto="Sobre nós" rota="/sobrenos"/></li>
@@ -33,6 +28,7 @@ return(
             </ul>
           </div>
         </div>
+        <br />
        
     
       </div>
@@ -46,10 +42,10 @@ return(
 
           <div className={styles.col}>
             <ul className={styles.social}>
-              <li><a className={styles.facebook} href="#"></a></li>
-              <li><a className={styles.twitter} href="#"></a></li>
-              <li><a className={styles.dribbble} href="#"></a></li>
-              <li><a className={styles.linkedin} href="#"></a></li>   
+              <li><a className={styles.facebook} href="#"><SiLinktree/></a></li>
+              <li><a className={styles.twitter} href="#"><SiInstagram/></a></li>
+              <li><a className={styles.dribbble} href="#"><BsGithub/></a></li>
+              <li><a className={styles.linkedin} href="#"><BsLinkedin></BsLinkedin></a></li>   
             </ul>
           </div>
         </div>
