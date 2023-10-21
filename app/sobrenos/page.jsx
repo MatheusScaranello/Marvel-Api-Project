8ce1a39e8037d38bd78343c606e0ee480b6ae944
MatheusScaranello <matheusscaranello.564@gmail.com>
2023-10-21 16:46:52 -0300

    carrrosel

--- app/sobrenos/page.jsx
+++ app/sobrenos/page.jsx
@@ -1,21 +1,48 @@
-import styles from './sobrenos.module.css'
-import IntegranteCash from '../components/integranteCash/IntegranteCash'
+"use client";
+
+import React, { useState } from 'react';
+import styles from './sobrenos.module.css';
+import IntegranteCash from '../components/integranteCash/IntegranteCash';
+import {BsFillCaretLeftFill, BsFillCaretRightFill} from 'react-icons/bs';
+import integrantes from '@/data/Integrantes';
 
 const Sobrenos = () => {
-    return (
-     
-        <div className={styles.container}>
-            <h1 className={styles.title}>Sobre nós</h1>
-            <div className={styles.cardContainer}>
-                <IntegranteCash nome="Matheus Scaranello" cargo="TechLeader" foto="https://i.imgur.com/8K96q0J.jpg" description="João é o CTO da empresa e é responsável por gerenciar a equipe" />
-                <IntegranteCash nome="Caique Naimi" cargo="CTO" foto="https://i.imgur.com/174eKbW.jpg" description="João é o CTO da empresa e é responsável por gerenciar a equipe e as tarefas." />
-                <IntegranteCash nome="Gabriel Canrin" cargo="CFO" foto="https://i.imgur.com/iO9H6V1.jpg" description="Maria é o CFO da empresa e é responsável por gerenciar a equipe e as tarefas." />
-                <IntegranteCash nome="Gabriel Picirili" cargo="CIO" foto="https://i.imgur.com/GUvnMes.jpg" description="José é o CIO da empresa e é responsável por gerenciar a equipe e as tarefas." />
-                <IntegranteCash nome="Marina Elis" cargo="CIO" foto="https://i.imgur.com/EdCWCfd.jpg" description="José é o CIO da empresa e é responsável por gerenciar a equipe e as tarefas." />
-                <IntegranteCash nome="Stefanny oliveira" cargo="CFO" foto="https://i.imgur.com/KQcoF2u.jpg" description="Maria é o CFO da empresa e é responsável por gerenciar a equipe e as tarefas." />
-            </div>
-        </div>
-    )
-}
 
-export default Sobrenos
+  const [IntegranteId, setIntegranteId] = useState(1);
+
+  const nextIntegrant = () => {
+    if (IntegranteId < 4) {
+      setIntegranteId(IntegranteId + 1);
+      return;
+    }else {
+        setIntegranteId(1);
+    }
+  };
+
+  const anteriorIntegrant = () => {
+    if (IntegranteId > 1) {
+      setIntegranteId(IntegranteId - 1);
+      return;
+    }
+    setIntegranteId(4);
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Sobre nós</h1>
+      <div className={styles.carouselContainer}>
+        <div className={styles.carouselSlide}>
+            <IntegranteCash {...integrantes[IntegranteId - 1]}/>
+            <IntegranteCash {...integrantes[IntegranteId]}/>
+            <IntegranteCash {...integrantes[IntegranteId + 1]}/>
+        </div>
+      </div>
+      <div className={styles.controls}>
+        <button onClick={anteriorIntegrant}><BsFillCaretLeftFill/></button>
+        <button onClick={nextIntegrant}><BsFillCaretRightFill/></button>
+      </div>
+    </div>
+  );
+};
+
+export default Sobrenos;
---
+++ data/Integrantes.js
@@ -0,0 +1,46 @@
+const integrantes = [
+    {
+        id: 0,
+      nome: "Matheus Scaranello",
+      cargo: "TechLeader",
+      foto: "https://i.imgur.com/8K96q0J.jpg",
+      description: "Matheus é o TechLeader da equipe e é responsável por gerenciar a equipe e programar"
+    },
+    {
+        id: 1,
+      nome: "Caique Naimi",
+      cargo: "Membro",
+      foto: "https://i.imgur.com/174eKbW.jpg",
+      description: "Caique é membro da equipe e é responsável por programar."
+    },
+    {
+        id: 2,
+      nome: "Gabriel Canrin",
+      cargo: "Membro",
+      foto: "https://i.imgur.com/iO9H6V1.jpg",
+      description: "Gabriel é membro da equipe e é responsável por programar."
+    },
+    {
+        id: 3,
+      nome: "Gabriel Picirili",
+      cargo: "Membro",
+      foto: "https://i.imgur.com/GUvnMes.jpg",
+      description: "Gabriel é o membro da equipe e é responsável por programar."
+    },
+    {
+        id: 4,
+      nome: "Marina Elis",
+      cargo: "Membro",
+      foto: "https://i.imgur.com/EdCWCfd.jpg",
+      description: "Marina é o membro da equipe e é responsável por programar."
+    },
+    {
+        id: 5,
+      nome: "Stefanny Oliveira",
+      cargo: "Membro",
+      foto: "https://i.imgur.com/KQcoF2u.jpg",
+      description: "Stefanny é o membro da equipe e é responsável por programar."
+    }
+  ];
+
+    export default integrantes;
