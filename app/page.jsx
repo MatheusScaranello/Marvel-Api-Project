9020eedc94fd9a835bc52b0d14f8d12ecb784807
Marinamorais <Marinamorais.564@gmail.com>
2023-10-22 10:50:16 -0300

    new commit

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import styles from "./page.module.css"
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
 
+
 const home = () => {
   return (<>
     <Header />
