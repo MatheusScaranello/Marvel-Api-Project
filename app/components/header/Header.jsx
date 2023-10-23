99aea23b8cd25b0ae3d726a6e5ae92aa1e2328fb
marinamorais <marinamorais.564@gmail.com>
2023-10-23 15:06:16 -0300

    header finish

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -1,10 +1,22 @@
+import teste from '@/app/createCharacters/page'
 import styles from './header.module.css'
+import NavLink from '@/app/components/navLink/NavLink'
+
 
 const Header = () => {
     return (
         <div className={styles.header}>
-            <div className={styles.marvel}><p className={styles.marvelTitle}>MARVEL</p></div>
-            <div className={styles.studios}><p className={styles.studiosTitle}>Studios</p></div>
+            
+            <div className={styles.imagem}>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Marvel_Studios_2016_logo.svg/2560px-Marvel_Studios_2016_logo.svg.png" alt="logo" height={50} width={190} />
+            </div>
+            <div className={styles.link}>
+             <li><NavLink texto="Home" rota="/"/></li>
+            <li><NavLink texto="Sobre nós" rota="/sobrenos"/></li>
+            <li><NavLink texto="Personagens" rota="/searchPage"/></li>
+            <li><NavLink texto="Crie personagens" rota="/createCharacters"/></li> 
+            </div>
+          
         </div>
     )
     }
