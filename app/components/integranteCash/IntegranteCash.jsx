5da630241464b96098d155bf6ae046f4ba00a8d4
marinamorais <marinamorais.564@gmail.com>
2023-10-23 10:47:06 -0300

    insta

--- app/components/integranteCash/IntegranteCash.jsx
+++ app/components/integranteCash/IntegranteCash.jsx
@@ -1,16 +1,16 @@
-import styles from "./integranteCash.module.css"
+import styles from "./integranteCash.module.css";
 
-
-function integranteCash({ nome, cargo, foto, description }) {
-    return (
-        
-        <div className={styles.card}>
-            <img className={styles.foto} src={foto} alt={nome} />
-            <h2 className={styles.nome}>{nome}</h2>
-            <h3 className={styles.cargo}>{cargo}</h3>
-            <p className={styles.description}>{description}</p>
-        </div>
-    )
+function integranteCash({ nome, cargo, foto, description, insta }) {
+  return (
+    <div className={styles.card}>
+      <a href={insta} target="_blank">
+        <img className={styles.foto} src={foto} alt={nome} />
+        <h2 className={styles.nome}>{nome}</h2>
+        <h3 className={styles.cargo}>{cargo}</h3>
+        <p className={styles.description}>{description}</p>
+      </a>
+    </div>
+  );
 }
 
-export default integranteCash
+export default integranteCash;
