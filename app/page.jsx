55106c0c54c8cd845983207d900c6d2d34aefde3
marinamorais <marinamorais.564@gmail.com>
2023-10-23 15:12:32 -0300

    new commit

--- app/page.jsx
+++ app/page.jsx
@@ -7,6 +7,7 @@ import Footer from "./components/footer/Footer"
 const home = () => {
   return (<>
     <Header />
+    
     <div className={styles.container}>
     <div className={styles.introducao}>
           <h1 className={`${styles.bemVindo} ${styles.fade}`}>Seja bem vindo!</h1>
