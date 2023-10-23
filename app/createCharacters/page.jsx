b2af98ed49c9fc391ac5271fc74ad648275d0191
marinamorais <marinamorais.564@gmail.com>
2023-10-23 07:42:37 -0300

    new commit

--- app/createCharacters/page.jsx
+++ app/createCharacters/page.jsx
@@ -1,4 +1,5 @@
 import CreateCharacters from "../components/createCharacters/CreateCharacters"
+import styles from"../createCharacters/createCharacters.module.css"
 import Header from "../components/header/Header"
 import Footer from "../components/footer/Footer"
 
