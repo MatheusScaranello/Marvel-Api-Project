c539bee2be44b5730006ead5d0e8757f384af7dc
MatheusScaranello <matheusscaranello.564@gmail.com>
2023-10-26 22:26:20 -0300

    adicionar personagem

--- app/createCharacters/page.jsx
+++ app/createCharacters/page.jsx
@@ -8,83 +8,104 @@ import { useState } from "react"
 
 
 export default function createCharacters() {
-    const [apiData, setApiData] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
-    const [name, setName] = useState("");
-    const [characterId, setCharacterId] = useState("");
+  const [apiData, setApiData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [name, setName] = useState("");
+  const [characterId, setCharacterId] = useState("");
+  const [newCharacter, setNewCharacter] = useState("");
+  const [id, setId] = useState(0);
 
-    const handleSearch = async () => {
-        try {
-          const dados = await getCharacters(searchTerm);
-          setApiData(dados);
-          setFlag(true);
-          setNumCharacters(10);
-        } catch (error) {
-          console.error(error);
-        }
-      };    
+  function generateId() {
+    setId(id + 1)
+    return id
+  }
 
-      const handleKeyPress = (e) => {
-        if (e.key === "Enter") {
-          handleSearch();
-        }
-      };
-
-      const handleNameChange = (characterId, name) => {
-        const updatedData = apiData.map((item) => {
-            if (item.id === characterId) {
-                return { ...item, name };
-            }
-            return item;
-            });
-            setApiData(updatedData);
-            setName(name);
-            setCharacterId(characterId);
-      };
-
-      const handleRemove = (id) => {
-        const updatedData = apiData.filter((item) => item.id !== id);
-        setApiData(updatedData);
-      }
-      
-
-    
-
-      return (
-        
-        <div className="App">
-            <div className={styles.inpts}>
-          <div className={styles.inputcontainer}>
-            <input
-              type="text"
-              placeholder="Procurar"
-              className={styles.input}
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              onKeyPress={handleKeyPress}
-            />
-            <button type="button" className={styles.btn} onClick={handleSearch}>
-              Procurar 🔎
-            </button>
-            <input
-                  type="text"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-          </div>
-        </div>
-          <h1>Lista de Personagens</h1>
-          <ul>
-            {apiData.map((character) => (
-              <li key={character.id}>
-                {character.name}
-                <button
-                  onClick={() => handleNameChange(character.id, name)} > salvar </button>
-                <button
-                    onClick={() => handleRemove(character.id)} > remover </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      );
+  const handleSearch = async () => {
+    try {
+      const dados = await getCharacters(searchTerm);
+      setApiData(dados);
+      setFlag(true);
+      setNumCharacters(10);
+    } catch (error) {
+      console.error(error);
     }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  const handleNameChange = (characterId, name) => {
+    const updatedData = apiData.map((item) => {
+      if (item.id === characterId) {
+        return { ...item, name };
+      }
+      return item;
+    });
+    setApiData(updatedData);
+    setName(name);
+    setCharacterId(characterId);
+  };
+
+  const handleRemove = (id) => {
+    const updatedData = apiData.filter((item) => item.id !== id);
+    setApiData(updatedData);
+  };
+
+  const Adicionar = () => {
+    generateId()
+    const newCharacter = { id: id, name };
+    const updatedData = [...apiData, newCharacter];
+    setApiData(updatedData);
+    setName("");
+    setCharacterId("");
+  };
+
+
+
+
+  return (
+
+    <div className="App">
+      <div className={styles.inpts}>
+        <div className={styles.inputcontainer}>
+          <input
+            type="text"
+            placeholder="Procurar"
+            className={styles.input}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyPress={handleKeyPress}
+          />
+          <button type="button" className={styles.btn} onClick={handleSearch}>
+            Procurar 🔎
+          </button>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="button" onClick={Adicionar}>
+            Adicionar
+          </button>
+
+        </div>
+      </div>
+      <h1>Lista de Personagens</h1>
+      <ul>
+        {apiData.map((character) => (
+          <li key={character.id}>
+            {character.name}
+            <button
+              onClick={() => handleNameChange(character.id, name)} > salvar </button>
+            <button
+              onClick={() => handleRemove(character.id)} > remover </button>
+          </li>
+        ))}
+      </ul>
+
+    </div>
+  );
+}
