d94638a0f2989e7630edd00174bafce8fd5a4000
Caique <caiquenaimi.564@gmail.com>
2023-10-30 09:29:54 -0300

    finish sobre nos

--- app/components/integranteCash/IntegranteCash.jsx
+++ app/components/integranteCash/IntegranteCash.jsx
@@ -4,10 +4,14 @@ function integranteCash({ nome, cargo, foto, description, insta }) {
   return (
     <div className={styles.card}>
       <a href={insta} target="_blank">
-        <img className={styles.foto} src={foto} alt={nome} />
-        <h2 className={styles.nome}>{nome}</h2>
-        <h3 className={styles.cargo}>{cargo}</h3>
-        <p className={styles.description}>{description}</p>
+        <div className={styles.card_image}>
+          <img className={styles.foto} src={foto} alt={nome} />
+        </div>
+        <div className={styles.card_content}>
+          <h2 className={styles.card_title}>{nome}</h2>
+          <h3 className={styles.card_name}>{cargo}</h3>
+          <p className={styles.card_text}>{description}</p>
+        </div>
       </a>
     </div>
   );
