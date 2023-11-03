492ceb61c907e5d6ccc41d6c94b90901a1b9113b
MatheusScaranello <matheusscaranello.564@gmail.com>
2023-11-03 12:18:06 -0300

    aa

--- app/components/NavLink/NavLink.jsx
+++ app/components/NavLink/NavLink.jsx
@@ -2,10 +2,8 @@
 import styles from "./navLink.module.css"
 import Link from "next/link"
 
-const NavLink = ({rota, texto}) => {
+export const NavLink = ({rota, texto}) => {
     return (
             <Link href={rota} className={styles.navLink}>{texto}</Link>
     )
 }
-
-export default NavLink;
