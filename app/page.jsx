ac6d7fd075477611be025b1f7972a1e390ee7288
Caique <caiquenaimi.564@gmail.com>
2023-11-03 22:26:13 -0300

    Add carousel to homepage and update dependencies

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,19 @@
+"use client"
 import styles from "./page.module.css"
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
 import NavLink from "./components/NavLink/NavLink"
+import { CCarouselItem } from '@coreui/react'
+import { CCarousel } from '@coreui/react'
+import { CImage } from '@coreui/react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 const home = () => {
   return (
     <>
       <Header />
+
       <div className={styles.fundo}>
         <div className={styles.container}>
           <div className={styles.introducao}>
@@ -17,6 +24,26 @@ const home = () => {
             <p className={styles.text}>A Marvel Comics, ou simplesmente Marvel, é uma das principais editoras de histórias em quadrinhos do mundo. Fundada em 1939 como Timely Publications e posteriormente conhecida como Atlas Comics, a empresa se tornou Marvel Comics na década de 1960. Sob a liderança de figuras icônicas como Stan Lee, Jack Kirby e Steve Ditko, a Marvel revolucionou a indústria de quadrinhos com a criação de personagens memoráveis, como o Homem-Aranha, X-Men, Quarteto Fantástico, Hulk, Thor, Homem de Ferro e os Vingadores.</p>
           </div>
           <div>
+            <div className={styles.carousel}>
+              <CCarousel
+                controls
+                transition="crossfade"
+                animateIn={true}
+                animateOut={true}
+                autoSlide={true}
+                autoSlideInterval={3000}
+              >
+                <CCarouselItem>
+                  <CImage className="d-block w-100" src={"https://assets-global.website-files.com/5dfd2203d08cf403964198c2/62bb241ed181683be780cd45_6182d7040018fc3f269a3116_marvel-characters-p-1600.jpeg"} alt="slide 1" />
+                </CCarouselItem>
+                <CCarouselItem>
+                  <CImage className="d-block w-100" src={"https://www.emp.ie/dw/image/v2/BBQV_PRD/on/demandware.static/-/Library-Sites-EMPSharedLibrary/default/dwe169dcbd/100151_MT_Marvel.jpg?sw=768"} alt="slide 2" />
+                </CCarouselItem>
+                <CCarouselItem>
+                  <CImage className="d-block w-100" src={"https://wallpapers.com/images/featured/marvel-comic-book-background-0exuprkk5cwj6ail.webp"} alt="slide 3" />
+                </CCarouselItem>
+              </CCarousel>
+            </div>
             <h2 className={`${styles.incons2} ${styles.fade}`}>CONHEÇA NOSSO SITE</h2>
           </div>
           <div className={styles.icons}>
