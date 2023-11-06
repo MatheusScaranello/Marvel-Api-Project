1facbb6750efd1e54371eaee7dded3d2ae347aa6
MatheusScaranello <matheusscaranello.564@gmail.com>
2023-11-05 23:23:25 -0300

    Add carousel controls and fix styles

--- app/sobrenos/page.jsx
+++ app/sobrenos/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import styles from './sobrenos.module.css';
 import IntegranteCash from '../components/integranteCash/IntegranteCash';
 import integrantes from '@/data/Integrantes';
@@ -7,9 +7,11 @@ import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import {BsFillCaretRightFill, BsFillCaretLeftFill} from 'react-icons/bs';
 
 const Sobrenos = () => {
   const swiperRef = useRef(null);
+  const [slideAtual, setSlideAtual] = useState(0);
 
   useEffect(() => {
     const swiper = swiperRef.current;
@@ -45,6 +47,20 @@ const Sobrenos = () => {
     };
   }, []);
 
+  const proximoSlide = () => {
+    setSlideAtual(slideAtual + 1);
+    if (slideAtual > 4) {
+      setSlideAtual(0);
+    }
+  };
+
+  const anteriorSlide = () => {
+    setSlideAtual(slideAtual - 1);
+    if (slideAtual < 0) {
+      setSlideAtual(4);
+    }
+  };
+
   return (
     <>
       <Header />
@@ -69,24 +85,17 @@ const Sobrenos = () => {
         </Swiper>
          </div>
          <div className={styles.container2}>
-         <Swiper
-          spaceBetween={200}
-          slidesPerView={6}
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={(swiper) => (swiperRef.current = swiper)}
-        >
-          <div className={styles.carouselContainer}>
-            <div className={styles.carouselSlide}>
-              {integrantes.map((integrante, index) => (
-                <SwiperSlide key={index}>
-                  <IntegranteCash {...integrante} />
-                </SwiperSlide>
-              ))}
-            </div>
-          </div>
-        </Swiper>
-          </div> 
+         <div className={styles.carouselContainer}>
+        <div className={styles.carouselSlide}>
+            <IntegranteCash {...integrantes[slideAtual]}/>
+        </div>
       </div>
+      <div className={styles.controls}>
+        <button className={styles.button} onClick={anteriorSlide}><BsFillCaretLeftFill/></button>
+        <button className={styles.button} onClick={proximoSlide}><BsFillCaretRightFill/></button>
+      </div>
+          </div>
+          </div>
       <Footer />
     </>
   );
